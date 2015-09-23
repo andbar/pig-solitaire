3c0c087ece3420ed1b67c4deb7133bddc6b071dc
andbar <barringera@gmail.com>
2015-09-22 22:16:20 -0400

    Pig Game is fully functional

--- pig.py
+++ pig.py
@@ -1,6 +1,8 @@
 import random
 
+
 class Dice:
+
 
     def __init__(self):
         self.possible_rolls = [1, 2, 3, 4, 5, 6]
@@ -8,6 +10,7 @@ class Dice:
     def roll_result(self):
         result = random.choice(self.possible_rolls)
         return result
+
 
 dice = Dice()
 
@@ -19,14 +22,17 @@ class Player:
         self.dice = dice
 
     def player_continue(self):
-        if input("Roll or hold? Enter 'r' or 'h' ") == "r":
+        player_response = input("Roll or hold? Enter 'r' or 'h' ")
+        if player_response.lower() == 'r':
             return True
-        else:
+        elif player_response.lower() == 'h':
             return False
+        else:
+            print("That's not an 'r' or an 'h'. Please try again.")
+            self.player_continue()
 
     def player_roll(self):
         return dice.roll_result()
-
 
     def player_turn(self):
         turn_score = 0
@@ -61,6 +67,7 @@ class Player:
                     print("You added {} points to your score this turn.".format(turn_score))
                     player_will_roll = False
         return turn_score
+
 
 player = Player()
 
@@ -114,33 +121,52 @@ class AI:
                     ai_will_roll = False
         return turn_score
 
+
 ai = AI()
 
 
 class Game:
 
-    def __init___(self, player, ai, rounds=7):
-        self.player1 = player
-        self.player2 = ai
-        self.players = [player1, player2]
+    def __init__(self, player1=player, player2=ai, rounds=7):
+        self.player1 = player1
+        self.player2 = player2
         self.rounds = rounds
 
 
-    def game_start(self):
-        print("Welcome to Pig Solitaire!")
+    def update_player_scores(self, round_scores): #tracks the current score of players
+        player.current_score += round_scores[0]
+        ai.current_score += round_scores[1]
 
-        pass
-
-    def player_scores(self): #tracks the current score of player
-        pass
-
-    def round_scores(self): #gives the scores from that round
+    def game_round(self): #plays a round and returns score for each player
         player1_round_score = player.player_turn()
         player2_round_score = ai.ai_turn()
-        return [player1_round_score, player2_round_score]
-
+        round_scores = [player1_round_score, player2_round_score]
         return round_scores
-        pass
 
-    def game_finish(self):
-        pass
+    def game_play(self):
+        print("Welcome to The Game of Pig\n")
+        print("You will take turns with another player, rolling a dice to accumulate points.")
+        print("While it is your turn, you may roll as many times as you'd like to collect points on that turn.")
+        print("However, if you roll a 1, you lose all points from that current turn and your turn is over.")
+        print("To keep the points collected on your turn, choose hold rather than rolling again "
+              "and the current points will be added to your total.")
+        print("Each player will have {} turns. You get to go first. Let's get started!".format(self.rounds))
+        current_round = 1
+        while current_round <= self.rounds:
+            print("\nStarting Turn {} of {}".format(current_round, self.rounds))
+            print("Current total points: You - {}  Player 2 - {}\n".format(player.current_score, ai.current_score))
+            round_scores = self.game_round()
+            self.update_player_scores(round_scores)
+            current_round += 1
+        final_score = [player.current_score, ai.current_score]
+        if player.current_score > ai.current_score:
+            print("\nCongratulations! You won, {} to {}.".format(player.current_score, ai.current_score))
+        elif player.current_score < ai.current_score:
+            print("\nBummer, you lost, {} to {}.".format(player.current_score, ai.current_score))
+        elif player.current_score == ai.current_score:
+            print("\nLooks like a tie! {} to {}.".format(player.current_score, ai.current_score))
+
+
+game = Game()
+
+#game.game_play()
